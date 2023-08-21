1c55240dfa658d6d29731c27a5fb32b5724becbf
André Zanghelini <An-dz@users.noreply.github.com>
2023-08-21 13:38:50 -0300

    Use tuples for object coordinates

--- src/objects/line.py
+++ src/objects/line.py
@@ -4,4 +4,4 @@ from objects.geometricObject import Coordinate, GeometricObject
 class Line(GeometricObject):
     """A line consists of two points in space"""
     def __init__(self, name: str, start_point: Coordinate, end_point: Coordinate) -> None:
-        super(Line, self).__init__(name, "Line", [ start_point, end_point ])
+        super(Line, self).__init__(name, "Line", (start_point, end_point))
--- src/objects/wireframe.py
+++ src/objects/wireframe.py
@@ -3,7 +3,7 @@ from objects.geometricObject import Coordinate, GeometricObject
 
 class Wireframe(GeometricObject):
     """A Wireframe consists of multiple points in space"""
-    def __init__(self, name: str, points: list[Coordinate]) -> None:
+    def __init__(self, name: str, points: tuple[Coordinate, ...]) -> None:
         if len(points) < 3:
             raise ValueError("Wireframe requires more than 2 points")
 
