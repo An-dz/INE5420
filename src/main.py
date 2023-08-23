cb3fa30b19fbc43d78105f9df3ef72c69a3e845f
André Zanghelini <An-dz@users.noreply.github.com>
2023-08-22 22:48:13 -0300

    Make sure icons always load

--- src/main.py
+++ src/main.py
@@ -1,11 +1,20 @@
+import os
 import sys
 from PyQt6 import QtWidgets
+from PyQt6.QtGui import QIcon
 
 from ui.mainWindow import MainWindow
 
 
+script_dir = os.path.dirname(__file__)
 app = QtWidgets.QApplication(sys.argv)
 
-window = MainWindow()
+icons = {
+    "Point": QIcon(os.path.join(script_dir, "assets/point.png")),
+    "Line": QIcon(os.path.join(script_dir, "assets/line.png")),
+    "Wireframe": QIcon(os.path.join(script_dir, "assets/wireframe.png")),
+}
+
+window = MainWindow(icons)
 window.show()
 app.exec()
