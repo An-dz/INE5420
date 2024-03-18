a8b27ced7ccdb38db38a44c0f39e2623fd1946a1
André Zanghelini <An-dz@users.noreply.github.com>
2024-03-18 13:40:21 -0300

    Draw window on viewport with correct transform

--- src/viewport.py
+++ src/viewport.py
@@ -9,17 +9,34 @@ class Viewport:
         self._window = window
         self._size = size
         self._graphic_scene = QtWidgets.QGraphicsScene()
+        drawing_color = QtGui.QColor("black")
+        self._pen = QtGui.QPen(drawing_color)
+        self._pen.setWidth(2)
+        self._brush = QtGui.QBrush(drawing_color)
 
     def getScene(self) -> QtWidgets.QGraphicsScene:
         return self._graphic_scene
 
     def draw(self):
-        pen = QtGui.QPen(QtGui.QColor("black"))
-        pen.setWidth(2)
         objects = self._window.getVisibleObjects()
         for obj in objects:
-            coords = obj.getCoordinates()
-            if obj.getType != "Point":
-                self.getScene().addLine(coords[0][0], coords[0][1], coords[1][0], coords[1][1], pen)
+            coords = iter(obj.getCoordinates())
+            last_point = next(coords)
+            if obj.getType() != "Point":
+                for cur_point in coords:
+                    self.getScene().addLine(
+                        self._window.getXW(last_point[0]) * self._size[0],
+                        self._window.getYW(last_point[1]) * self._size[1],
+                        self._window.getXW(cur_point[0]) * self._size[0],
+                        self._window.getYW(cur_point[1]) * self._size[1],
+                        self._pen
+                    )
+                    last_point = cur_point
             else:
-                self.getScene().addEllipse(coords[0][0], coords[0][1], 5, 5, pen, QtGui.QBrush(QtGui.QColor("black")))
+                self.getScene().addEllipse(
+                    self._window.getXW(last_point[0]) * self._size[0],
+                    self._window.getYW(last_point[1]) * self._size[1],
+                    5, 5,
+                    self._pen,
+                    self._brush
+                )
