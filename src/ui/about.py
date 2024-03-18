a9c3e350d7cb5dcdeb788306b78d487c9d8f0fed
André Zanghelini <An-dz@users.noreply.github.com>
2024-03-18 13:39:17 -0300

    Add comments for About window class

--- src/ui/about.py
+++ src/ui/about.py
@@ -4,11 +4,12 @@ from ui.generated.about import Ui_Dialog
 
 
 class AboutDialog(QtWidgets.QDialog, Ui_Dialog):
-    """docstring for AboutDialog"""
+    """A simple about screen just to show who made this"""
     def __init__(self, *args, **kwargs) -> None:
         super(AboutDialog, self).__init__(*args, **kwargs)
         self.setupUi(self)
         self.pushButton.clicked.connect(self.actionCloseButton)
 
     def actionCloseButton(self) -> None:
+        """Close the window when clicking the close button"""
         self.close()
