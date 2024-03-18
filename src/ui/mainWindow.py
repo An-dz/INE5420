7173bdb6f897b016c3c43e5e42756f5ad8e6c7f9
André Zanghelini <An-dz@users.noreply.github.com>
2024-03-18 18:24:53 -0300

    Add window movement with keypad (Blender-like)

--- src/ui/mainWindow.py
+++ src/ui/mainWindow.py
@@ -1,4 +1,6 @@
 from PyQt6 import QtWidgets
+from PyQt6.QtCore import Qt
+from PyQt6.QtGui import QKeyEvent
 from ui.generated.mainWindow import Ui_MainWindow
 from ui.about import AboutDialog
 from viewport import Viewport
@@ -23,6 +25,19 @@ class MainWindow(QtWidgets.QMainWindow, Ui_MainWindow):
         self.graphicsView.setScene(self._viewport.getScene())
         self.graphicsView.setSceneRect(0,0,self.graphicsView.height() - 2,self.graphicsView.width() - 2)
         self._viewport.draw()
+
+    def keyPressEvent(self, event: QKeyEvent | None) -> None:
+        if event:
+            numpad_mod = event.modifiers() & Qt.KeyboardModifier.KeypadModifier
+            if numpad_mod:
+                if event.key() == Qt.Key.Key_2:
+                    self.actionMoveDown()
+                elif event.key() == Qt.Key.Key_4:
+                    self.actionMoveLeft()
+                elif event.key() == Qt.Key.Key_6:
+                    self.actionMoveRight()
+                elif event.key() == Qt.Key.Key_8:
+                    self.actionMoveUp()
 
     def actionMoveLeft(self) -> None:
         self._window_obj.move(-20, 0)
